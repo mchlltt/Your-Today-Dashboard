30a92ac3ab707c194901491278a848ffc7774183
mchlltt <me@northwestern.edu>
2016-11-16 12:46:25 -0600

    Fix localStorage assignment, add news API and NYT Twitter stream

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
         var latLocal = localStorage.getItem('lat');
         var longLocal = localStorage.getItem('long');
 
-        // !!! This is broken. need to find the right way to determine if a key is set in localStorage.
-        if (nameLocal.length > 0 && latLocal.length > 0 && longLocal.length > 0) {
-            return false;
-        } else {
+        // This is broken. need to find the right way to determine if a key is set in localStorage.
+        // if (nameLocal.length > 0 && latLocal.length > 0 && longLocal.length > 0) {
+        //     return false;
+        // } else {
             return true;
-        }
+        // }
     };
 
     // Determine during pageload whether to display the form.
@@ -23,6 +23,7 @@ $(document).ready(function() {
         }
     };
 
+    // Run on pageload.
     hideForm();
 
 
@@ -37,14 +38,17 @@ $(document).ready(function() {
 
     // Get input from input boxes.
     $('#submit-button').on('click', function() {
+
         // Get name input.
         name = $('#username').val();
+        localStorage.setItem('name', name);
 
         // If the address was not automatically collected by the browser, geocode it.
         if (inputAddress) {
 
             // User's raw address.
             rawAddress = $('#location').val();
+            localStorage.setItem('rawAddress', rawAddress);
 
             // Initialize geocoder.
             var geocoder = new google.maps.Geocoder();
@@ -55,21 +59,25 @@ $(document).ready(function() {
                     // Save latitude and longitude.
                     lat = results[0].geometry.location.lat();
                     long = results[0].geometry.location.lng();
+
+                    localStorage.setItem('lat', lat);
+                    localStorage.setItem('long', long);
+
                 } else {
                     // Log error code. We'll need to have an error display for the user but this works for now.
                     $('#form-submit-message').text("Geocode was not successful for the following reason: " + status);
                 }
             });
+        } else {
+            localStorage.setItem('lat', lat);
+            localStorage.setItem('long', long);
         }
 
-        // Update local storage values.
-        localStorage.setItem('name', name);
-        localStorage.setItem('rawAddress', rawAddress);
-        localStorage.setItem('lat', lat);
-        localStorage.setItem('long', long);
-
+        // Clear form.
         $('#location').val('');
         $('#username').val('');
+
+        // Indicate success.
         $('#form-submit-message').text('Sucess!');
 
         // Don't refresh.
@@ -80,7 +88,7 @@ $(document).ready(function() {
     $('#auto-locate').on('click', function() {
         if (navigator.geolocation) {
             // Hand the results to savePosition.
-            navigator.geolocation.getCurrentPosition(savePosition);
+            navigator.geolocation.getCurrentPosition(savePosition, positionError);
         } else {
             // Very basic error handling for now.
             $('#form-submit-message').text('Error getting address.');
@@ -102,6 +110,9 @@ $(document).ready(function() {
         inputAddress = false;
     };
 
+    positionError = function() {
+        $('#form-submit-message').text('Error getting address.');
+    };
 
 
 
