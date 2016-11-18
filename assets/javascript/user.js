989cba2883d8ef65e5afa69b4690a0eb4cd7c540
mchlltt <me@northwestern.edu>
2016-11-18 15:22:46 -0600

    Get/set Firebase data, get weather with dynamic location, add ability to change input

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -1,93 +1,77 @@
 $(document).ready(function() {
 
-    // Function for use when determining whether to display the form asking for user info.
-    isUserInfoNeeded = function() {
-        var nameLocal = localStorage.getItem('name');
-        var latLocal = localStorage.getItem('lat');
-        var longLocal = localStorage.getItem('long');
-
-        // This is broken. need to find the right way to determine if a key is set in localStorage.
-        // if (nameLocal.length > 0 && latLocal.length > 0 && longLocal.length > 0) {
-        //     return false;
-        // } else {
-        return true;
-        // }
+    // Configure and initialize Firebase.
+    var config = {
+        apiKey: "AIzaSyDCO42TtlK2h-MzIpVt1qGGQR-AmpwQVS0",
+        authDomain: "your-today-dashboard.firebaseapp.com",
+        databaseURL: "https://your-today-dashboard.firebaseio.com",
+        storageBucket: "your-today-dashboard.appspot.com",
+        messagingSenderId: "840765761710"
     };
+    firebase.initializeApp(config);
 
-    // Determine whether to display the form.
-    hideForm = function() {
-        if (isUserInfoNeeded()) {
-            $('form').show();
-        } else {
-            $('form').hide();
-        }
-    };
+    // Assign the Firebase database to a variable.
+    var database = firebase.database();
 
-    // Check on pageload.
-    hideForm();
+    // Assign sub-level of the databse to variables.
+    var users = database.ref('users');
+    var displayNames = database.ref('displayNames');
+    var locations = database.ref('locations');
 
-    // Initialize user variables.
-    var name;
-    var rawAddress;
+    // Initialize variables that will be written to or read from Firebase.
+    var displayName;
+    var location;
     var lat;
     var long;
+    
+    // *** Hard-coded test username. This will have been received through log-in process.
+    var username = 'mchlltt';
 
-    // This variable indicates whether the address was input manually by the user and needs to be geocoded.
-    // It is true by default.
-    var inputAddress = true;
+    // Fake-simulating logging in.
+    // This event listener will be replaced with what actually happens when you log in successfully.
+    $('#login-button').on('click', function() {
 
-    // It is also true if the user has typed in the location box.
-    $('#location').on('keyup', function() {
-        inputAddress = true;
-    });
-
-    // Get input from input boxes.
-    $('#submit-button').on('click', function() {
-
-        // Get name input.
-        name = $('#username').val();
-        localStorage.setItem('name', name);
-
-        // If the address was not automatically collected by the browser, geocode it.
-        if (inputAddress) {
-
-            // User's raw address.
-            rawAddress = $('#location').val();
-            localStorage.setItem('rawAddress', rawAddress);
-
-            // Initialize geocoder.
-            var geocoder = new google.maps.Geocoder();
-
-            // Geocode raw address input.
-            geocoder.geocode({
-                'address': rawAddress
-            }, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    // Save latitude and longitude.
-                    lat = results[0].geometry.location.lat();
-                    long = results[0].geometry.location.lng();
-
-                    localStorage.setItem('lat', lat);
-                    localStorage.setItem('long', long);
-
-                } else {
-                    // Log error code. We'll need to have an error display for the user but this works for now.
-                    $('#form-submit-message').text("Geocode was not successful for the following reason: " + status);
-                }
-            });
-        } else {
-            localStorage.setItem('lat', lat);
-            localStorage.setItem('long', long);
-        }
-
-        // Clear form.
-        $('#location').val('');
-
-        // Indicate success.
-        $('#form-submit-message').text('Sucess!');
+        $('#login-button').hide();
+        // Check whether you need to show the form.
+        $.when(isUserInfoNeeded()).done(showForm);
 
         // Don't refresh.
         return false;
+    });
+    
+    // Check whether we have the form inputs already.
+    isUserInfoNeeded = function() {
+        // Check if a display name is already known for this username.
+        displayNames.child(username).once('value', function(snapshot) {
+            if (snapshot.exists()) {
+                displayName = snapshot.val().displayName;
+                $('#displayName').val(displayName);
+            }
+        });
+
+        // Check if a location is already known for this username.
+        return locations.child(username).once('value',function(snapshot) {
+            if (snapshot.exists()) {
+                location = snapshot.val().location;
+                $('#location').val(location);
+            }
+        });
+    };
+
+    // If we found that we have a display name and location already, hide the form.
+    showForm = function() {
+        if (displayName === undefined || location === undefined) {
+            $('.form').show();
+        } else {
+            $('.welcome').show();
+            $('.change').show();
+        }
+    };
+
+    $('#change-button').on('click', function() {
+        $('.form').show();
+        $('.welcome').hide();
+        $('.change').hide();
     });
 
     // Get location from 'Auto-Locate' button.
@@ -96,29 +80,116 @@ $(document).ready(function() {
             // Hand the results to savePosition.
             navigator.geolocation.getCurrentPosition(savePosition, positionError);
         } else {
-            // Very basic error handling for now.
-            $('#form-submit-message').text('Error getting address.');
+            // Very basic error handling for now. Indicates browser incompatibility.
+            console.log('Geolocation not possible.');
         }
 
         // Don't refresh.
         return false;
     });
 
+    // If auto-locate is successful, write it to the location input..
     savePosition = function(position) {
-        // Save results of geolocation.    
         lat = position.coords.latitude;
         long = position.coords.longitude;
 
-        // Write the truncated coordinates to the location input to show the user that it was completed successfully.
+        // Write the truncated coordinates to the location input.
         $('#location').val(lat.toFixed(3) + ',' + long.toFixed(3));
-
-        // Tell the program not to geocode ('#location').val() since we already have the full coordinates saved to lat and long.
-        // This will be overwritten back to true if the user types in the location box.
-        inputAddress = false;
     };
 
+    // If auto-locate fails, display error text.
     positionError = function() {
         $('#form-submit-message').text('Error getting address.');
     };
+
+
+    // When the form is submitted with the display name and location,
+    $('#submit-button').on('click', function() {
+        // Get contents of displayName input.
+        displayName = $('#displayName').val();
+
+        // Verify that a name was input before sending it to Firebase.
+        if (displayName.length > 0) {
+            var displayNameObj = {};
+            displayNameObj[username] = {
+                displayName: displayName
+            };
+            displayNames.update(displayNameObj);
+        }
+
+        // Get contents of location input.
+        location = $('#location').val();
+
+        // Verify that a location was input before geocoding & sending it to Firebase.
+        if (location.length > 0) {
+            // Initialize geocoder.
+            var geocoder = new google.maps.Geocoder();
+
+            // Geocode location input.
+            geocoder.geocode({
+                'address': location
+            }, function(results, status) {
+                // If geocoding was successful,
+                if (status == google.maps.GeocoderStatus.OK) {
+                    // Save latitude and longitude.
+                    lat = results[0].geometry.location.lat();
+                    long = results[0].geometry.location.lng();
+
+                    // Write location and lat/long to Firebase.            
+                    var locationObj = {};
+                    locationObj[username] = {
+                        location: location,
+                        lat: lat,
+                        long: long
+                    };
+                    locations.update(locationObj);
+
+                    // Hide form and show change button only if geocoding was successful.
+                    $('.form').hide();
+                    $('.welcome').show();
+                    $('.change').show();
+
+                    // Display success message.
+                    console.log('Name and location set!');
+
+                } else {
+                    // Display a basic error code. Need better handling but this works for now.
+                    console.log("Geocode was not successful for the following reason: " + status);
+                }
+            });
+        }
+
+        return false;
+    });
+
+
+    // Listen for changes in name.
+    displayNames.on('value', function(snapshot) {
+        displayName = snapshot.child(username).val().displayName;
+        $('#hello').text('Hello ' + displayName + '!');
+    });
+
+    
+    // Listen for changes in location.
+    locations.on('value', function(snapshot) {
+        lat = snapshot.child(username).val().lat;
+        long = snapshot.child(username).val().long;
+    });
+
+    // Fetch weather.
+    $('#weather-button').on('click', function() {
+        var apiKey = '0b3cbdf73e99584a55eddd1b6bd851f6';
+        var url = 'https://api.forecast.io/forecast/';
+
+        $.getJSON(url + apiKey + "/" + lat + "," + long + "?callback=?", function(data) {
+            // console.log(data);
+            $('#weather').html('The temperature: ' + data.currently.temperature + ' degrees');
+            $('#summary').html(data.currently.summary);
+        });
+        //https://api.forecast.io/forecast/0b3cbdf73e99584a55eddd1b6bd851f6
+
+        return false;
+
+    });
 
 });
